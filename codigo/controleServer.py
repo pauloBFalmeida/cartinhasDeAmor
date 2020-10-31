d088ea9d9b6a529fed44c0c4b56554e9efffc402
Paulo B F Almeida <paulobfalmeida@gmail.com>
2020-10-31 18:15:20 -0300

    Update controleServer.py

--- codigo/controleServer.py
+++ codigo/controleServer.py
@@ -17,36 +17,6 @@ class ControleServer():
     def addJogadorIdIp(self, id, ip):
         self.__jogadores_ip[id] = ip
 
-    def __jogadorTurnoEnviar(jogadorTurno, texto):
-        id = jogadorTurno.getId()
-        ip = self.__jogadores_ip[id]
-        self.interRede.enviar(ip, texto)
-
-    def __jogadorTurnoEnviarLista(jogadorTurno, l):
-        id = jogadorTurno.getId()
-        ip = self.__jogadores_ip[id]
-        self.interRede.enviar(ip, l)
-
-    def __enviar(self, texto):
-        for id in self.__jogadores_ip:
-            ip = self.__jogadores_ip[id]
-            self.interRede.enviar(ip, texto)
-
-    def __enviarJogador(self, j):
-        for id in self.__jogadores_ip:
-            ip = self.__jogadores_ip[id]
-            self.interRede.enviar(ip, j)
-
-    def __enviarCarta(self, c):
-        for id in self.__jogadores_ip:
-            ip = self.__jogadores_ip[id]
-            self.interRede.enviar(ip, c)
-
-    def __enviarLista(self, l):
-        for id in self.__jogadores_ip:
-            ip = self.__jogadores_ip[id]
-            self.interRede.enviar(ip, l)
-    
     def iniciarRound(self):
         self.__enviar(self.__cmd+"iniciarRound")
 
@@ -65,7 +35,8 @@ class ControleServer():
 
     def jogadorEscolherCarta(self, jogadorTurno):
         self.__jogadorTurnoEnviar(jogadorTurno, self.__cmd+"jogadorEscolherCarta")
-        return None
+        index_carta = self.__esperarResposta()
+        return index_carta
 
     def alertarSobreCondessa(self, jogadorTurno):
         self.__jogadorTurnoEnviar(jogadorTurno, self.__cmd+"alertarSobreCondessa")
@@ -78,11 +49,13 @@ class ControleServer():
     def selecionaJogador(self, jogadorTurno, jogadores, siMesmo, fraseInicio):
         self.__jogadorTurnoEnviar(self.__cmd+"selecionaJogador"+":"+str(siMesmo)+":"+fraseInicio)
         self.__jogadorTurnoEnviarLista(jogadores)   #carta_jogada.getFraseInicio()
-        return None
+        index_jogador = self.__esperarResposta()
+        return index_jogador
 
     def selecionaValorGuarda(self, jogadorTurno):
         self.__jogadorTurnoEnviar(jogadorTurno, self.__cmd+"selecionaValorGuarda")
-        return None
+        id_carta = self.__esperarResposta()
+        return id_carta
 
     def resultadoGuarda(self, result):
         id1 = result.getId()
@@ -116,3 +89,38 @@ class ControleServer():
     def anunciarMorto(self, jogador):
         self.__enviar(self.__cmd+"anunciarMorto")
         self.__enviarJogador(jogador)
+
+    
+    def __jogadorTurnoEnviar(jogadorTurno, texto):
+        id = jogadorTurno.getId()
+        ip = self.__jogadores_ip[id]
+        self.interRede.enviar(ip, texto)
+
+    def __jogadorTurnoEnviarLista(jogadorTurno, l):
+        id = jogadorTurno.getId()
+        ip = self.__jogadores_ip[id]
+        self.interRede.enviar(ip, l)
+
+    def __enviar(self, texto):
+        for id in self.__jogadores_ip:
+            ip = self.__jogadores_ip[id]
+            self.interRede.enviar(ip, texto)
+
+    def __enviarJogador(self, j):
+        for id in self.__jogadores_ip:
+            ip = self.__jogadores_ip[id]
+            self.interRede.enviar(ip, j)
+
+    def __enviarCarta(self, c):
+        for id in self.__jogadores_ip:
+            ip = self.__jogadores_ip[id]
+            self.interRede.enviar(ip, c)
+
+    def __enviarLista(self, l):
+        for id in self.__jogadores_ip:
+            ip = self.__jogadores_ip[id]
+            self.interRede.enviar(ip, l)
+
+    def __esperarResposta(self):
+        return None
+    
